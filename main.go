45f8f99cab61937baabfabd217285d397559862e
outranker <outranker222@gmail.com>
2022-03-03 18:31:08 +0900

    testing chromedp

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"log"
+	"strings"
+
+	"github.com/chromedp/chromedp"
+)
 
 func main() {
-	fmt.Println("hi mom")
+	ctx, cancel := chromedp.NewContext(context.Background())
+	defer cancel()
+
+	var res string
+	err := chromedp.Run(ctx,chromedp.Navigate(`https://pkg.go.dev/time`), chromedp.Text(`.Documentation-overview`, &res, chromedp.NodeNotVisible))
+
+	if err != nil { 
+		log.Fatal(err)
+	}
+	log.Println(strings.TrimSpace(res))
 }
